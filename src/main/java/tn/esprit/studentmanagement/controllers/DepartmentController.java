a42a870e6ede49066e55fc8707b743897faa6e68
yosra2223 <yosraabdelkader44@gmail.com>
2025-12-07 18:01:47 +0100

    initial commit

--- src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
+++ src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
@@ -8,7 +8,7 @@ import tn.esprit.studentmanagement.services.IDepartmentService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/department") // ✔ CORRECTION
+@RequestMapping("/Department")
 @CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 public class DepartmentController {
