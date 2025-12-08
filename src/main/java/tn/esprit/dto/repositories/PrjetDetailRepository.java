d6e15b47825ebd07eaa65a9de64d48d77df71a7c
yosra2223 <yosraabdelkader44@gmail.com>
2025-12-08 23:54:17 +0100

    Premier commit de StudentsManagement-DevOps-main2

--- src/main/java/tn/esprit/dto/repositories/PrjetDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.dto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.dto.entities.ProjetDetail;
-
-@Repository
-public interface PrjetDetailRepository  extends JpaRepository<ProjetDetail, Long> {
-}
--- src/main/java/tn/esprit/dto/repositories/ProjetRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.dto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.dto.entities.Projet;
-
-@Repository
-public interface ProjetRepository extends JpaRepository<Projet, Long> {
-}
